75e2b52d4a78b27e021d26a4a7101ef6ea30306f
Chris Pulman <chris.pulman@yahoo.com>
2023-09-01 00:45:51 +0100

    Add Text

--- src/XamlLEDControl.WPF/XamlLED.cs
+++ src/XamlLEDControl.WPF/XamlLED.cs
@@ -25,13 +25,21 @@ public class XamlLED : ContentControl
     /// The led orientation property.
     /// </summary>
     public static readonly DependencyProperty LedOrientationProperty =
-        DependencyProperty.Register(nameof(LedOrientation), typeof(Orientation), typeof(XamlLED), new PropertyMetadata(Orientation.Horizontal));
+        DependencyProperty.Register(
+            nameof(LedOrientation),
+            typeof(Orientation),
+            typeof(XamlLED),
+            new PropertyMetadata(Orientation.Horizontal));
 
     /// <summary>
     /// The is true property.
     /// </summary>
     public static readonly DependencyProperty IsTrueProperty =
-        DependencyProperty.Register(nameof(IsTrue), typeof(bool), typeof(XamlLED), new PropertyMetadata(false, IsTruePropertyChanged));
+        DependencyProperty.Register(
+            nameof(IsTrue),
+            typeof(bool),
+            typeof(XamlLED),
+            new PropertyMetadata(false, IsTruePropertyChanged));
 
     /// <summary>
     /// The leds On property.
@@ -67,7 +75,11 @@ public class XamlLED : ContentControl
     /// The off opacity property.
     /// </summary>
     public static readonly DependencyProperty OffOpacityProperty =
-        DependencyProperty.Register(nameof(OffOpacity), typeof(double), typeof(XamlLED), new PropertyMetadata(0.4));
+        DependencyProperty.Register(
+            nameof(OffOpacity),
+            typeof(double),
+            typeof(XamlLED),
+            new PropertyMetadata(0.4));
 
     /// <summary>
     /// The active led property.
@@ -80,27 +92,90 @@ public class XamlLED : ContentControl
             new FrameworkPropertyMetadata
             {
                 BindsTwoWayByDefault = true,
-                DefaultValue = 0,
+                DefaultValue = -1,
                 PropertyChangedCallback = ActiveLedChanged
             });
 
+    /// <summary>
+    /// The text position property.
+    /// </summary>
+    public static readonly DependencyProperty TextPositionProperty =
+        DependencyProperty.Register(
+            nameof(TextPosition),
+            typeof(TextPosition),
+            typeof(XamlLED),
+            new PropertyMetadata(TextPosition.Center, TextPositionChanged));
+
+    /// <summary>
+    /// The text property.
+    /// </summary>
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register(
+            nameof(Text),
+            typeof(string),
+            typeof(XamlLED),
+            new PropertyMetadata(string.Empty, TextChange));
+
+    /// <summary>
+    /// The led size property.
+    /// </summary>
+    public static readonly DependencyProperty LEDSizeProperty =
+        DependencyProperty.Register(
+            nameof(LEDSize),
+            typeof(double),
+            typeof(XamlLED),
+            new PropertyMetadata(40d, LedSizeChange));
+
     private readonly List<Ellipse> _leds = new();
+    private readonly TextBlock _LedText = new();
+    private readonly StackPanel _ledStackPanel = new();
+    private readonly Grid _layoutRoot = new();
 
     /// <summary>
     /// Initializes a new instance of the <see cref="XamlLED"/> class.
     /// </summary>
-    public XamlLED() => Loaded += LoadLeds;
+    public XamlLED()
+    {
+        _layoutRoot.ColumnDefinitions.Add(new() { Width = GridLength.Auto });
+        _layoutRoot.ColumnDefinitions.Add(new() { Width = GridLength.Auto });
+        _layoutRoot.ColumnDefinitions.Add(new() { Width = GridLength.Auto });
+        _layoutRoot.RowDefinitions.Add(new() { Height = GridLength.Auto });
+        _layoutRoot.RowDefinitions.Add(new() { Height = GridLength.Auto });
+        _layoutRoot.RowDefinitions.Add(new() { Height = GridLength.Auto });
+        Content = _layoutRoot;
+        Grid.SetRow(_ledStackPanel, 1);
+        Grid.SetColumn(_ledStackPanel, 1);
+        Grid.SetColumnSpan(_ledStackPanel, 1);
+        Grid.SetRowSpan(_ledStackPanel, 1);
+        _layoutRoot.Children.Add(_ledStackPanel);
+
+        _LedText.HorizontalAlignment = HorizontalAlignment.Center;
+        _LedText.VerticalAlignment = VerticalAlignment.Center;
+        _LedText.TextAlignment = TextAlignment.Center;
+        _LedText.Text = Text;
+        _LedText.Foreground = Foreground;
+        _LedText.FontWeight = FontWeight;
+        _LedText.FontSize = FontSize;
+        _LedText.Margin = new Thickness(5);
+
+        Grid.SetRow(_LedText, 1);
+        Grid.SetColumn(_LedText, 1);
+        Grid.SetColumnSpan(_LedText, 1);
+        Grid.SetRowSpan(_LedText, 1);
+        _layoutRoot.Children.Add(_LedText);
+        Loaded += LoadLeds;
+    }
 
     /// <summary>
-    /// Gets or sets a value indicating whether this instance is true.
+    /// Gets or sets the text.
     /// </summary>
-    /// <value><c>true</c> if this instance is true; otherwise, <c>false</c>.</value>
-    [Description("Gets or sets a value indicating whether this instance is true, only valid when a single LED is used")]
-    [Category("Control")]
-    public bool IsTrue
+    /// <value>The text.</value>
+    [Description("Set the Text of the control")]
+    [Category("Layout")]
+    public string Text
     {
-        get => (bool)GetValue(IsTrueProperty);
-        set => SetValue(IsTrueProperty, value);
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
     }
 
     /// <summary>
@@ -112,6 +187,18 @@ public class XamlLED : ContentControl
     {
         get => (Orientation)GetValue(LedOrientationProperty);
         set => SetValue(LedOrientationProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets the text position.
+    /// </summary>
+    /// <value>The text position.</value>
+    [Description("Set the Text Position of the control")]
+    [Category("Layout")]
+    public TextPosition TextPosition
+    {
+        get => (TextPosition)GetValue(TextPositionProperty);
+        set => SetValue(TextPositionProperty, value);
     }
 
     /// <summary>
@@ -140,9 +227,21 @@ public class XamlLED : ContentControl
     }
 
     /// <summary>
+    /// Gets or sets the size of the led.
+    /// </summary>
+    /// <value>The size of the led.</value>
+    [Description("Set the LED size of the control, default is 40")]
+    [Category("Layout")]
+    public double LEDSize
+    {
+        get => (double)GetValue(LEDSizeProperty);
+        set => SetValue(LEDSizeProperty, value);
+    }
+
+    /// <summary>
     /// Gets or sets opacity of led in off mode.
     /// </summary>
-    [Description("Gets or sets opacity of leds in off mode.")]
+    [Description("Gets or sets opacity of leds in off mode, only used in multiple Led mode.")]
     [Category("Appearance")]
     public double OffOpacity
     {
@@ -151,12 +250,24 @@ public class XamlLED : ContentControl
     }
 
     /// <summary>
+    /// Gets or sets a value indicating whether this instance is true.
+    /// </summary>
+    /// <value><c>true</c> if this instance is true; otherwise, <c>false</c>.</value>
+    [Description("Gets or sets a value indicating whether this instance is true, only valid when a single LED is used")]
+    [Category("Control")]
+    public bool IsTrue
+    {
+        get => (bool)GetValue(IsTrueProperty);
+        set => SetValue(IsTrueProperty, value);
+    }
+
+    /// <summary>
     /// Gets or sets ative index of the leds
     /// Value 0 = nothing active
     /// Value 1 = first led active
     /// Value 2 = second led active etc.
     /// </summary>
-    [Description("Gets or sets a value indicating which instance is On, only valid when multiple On LED's are used")]
+    [Description("Gets or sets a value indicating which instance is On, only used in multiple Led mode")]
     [Category("Control")]
     public int ActiveLed
     {
@@ -164,40 +275,122 @@ public class XamlLED : ContentControl
         set => SetValue(ActiveLedProperty, value);
     }
 
+    private static void LedSizeChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is XamlLED led)
+        {
+            led.LEDSize = (double)e.NewValue;
+            led.LoadLeds(d, null!);
+        }
+    }
+
+    private static void TextChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is XamlLED led)
+        {
+            var text = e.NewValue.ToString();
+            led.Text = text!;
+            led._LedText!.Text = text;
+        }
+    }
+
+    private static void TextPositionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is XamlLED led && e.NewValue is TextPosition position)
+        {
+            led.TextPosition = position;
+            switch (position)
+            {
+                case TextPosition.Top:
+                    Grid.SetRow(led._LedText, 0);
+                    Grid.SetColumn(led._LedText, 1);
+                    Grid.SetColumnSpan(led._LedText, 1);
+                    Grid.SetRow(led._ledStackPanel, 1);
+                    Grid.SetColumn(led._ledStackPanel, 1);
+                    led._LedText.VerticalAlignment = VerticalAlignment.Top;
+                    led._LedText.HorizontalAlignment = HorizontalAlignment.Center;
+                    led._LedText.TextAlignment = TextAlignment.Center;
+                    break;
+
+                case TextPosition.Bottom:
+                    Grid.SetRow(led._LedText, 3);
+                    Grid.SetColumn(led._LedText, 1);
+                    Grid.SetColumnSpan(led._LedText, 1);
+                    Grid.SetRow(led._ledStackPanel, 1);
+                    Grid.SetColumn(led._ledStackPanel, 1);
+                    led._LedText.VerticalAlignment = VerticalAlignment.Bottom;
+                    led._LedText.HorizontalAlignment = HorizontalAlignment.Center;
+                    led._LedText.TextAlignment = TextAlignment.Center;
+                    break;
+
+                case TextPosition.Left:
+                    Grid.SetRow(led._LedText, 1);
+                    Grid.SetColumn(led._LedText, 0);
+                    Grid.SetColumnSpan(led._LedText, 1);
+                    Grid.SetRow(led._ledStackPanel, 1);
+                    Grid.SetColumn(led._ledStackPanel, 1);
+                    led._LedText.VerticalAlignment = VerticalAlignment.Center;
+                    led._LedText.HorizontalAlignment = HorizontalAlignment.Left;
+                    led._LedText.TextAlignment = TextAlignment.Left;
+                    break;
+
+                case TextPosition.Right:
+                    Grid.SetRow(led._LedText, 1);
+                    Grid.SetColumn(led._LedText, 3);
+                    Grid.SetColumnSpan(led._LedText, 1);
+                    Grid.SetRow(led._ledStackPanel, 1);
+                    Grid.SetColumn(led._ledStackPanel, 1);
+                    led._LedText.VerticalAlignment = VerticalAlignment.Center;
+                    led._LedText.HorizontalAlignment = HorizontalAlignment.Right;
+                    led._LedText.TextAlignment = TextAlignment.Right;
+                    break;
+
+                case TextPosition.Center:
+                    Grid.SetRow(led._LedText, 1);
+                    Grid.SetColumn(led._LedText, 1);
+                    Grid.SetColumnSpan(led._LedText, 1);
+                    Grid.SetRow(led._ledStackPanel, 1);
+                    Grid.SetColumn(led._ledStackPanel, 1);
+                    led._LedText.VerticalAlignment = VerticalAlignment.Center;
+                    led._LedText.HorizontalAlignment = HorizontalAlignment.Center;
+                    led._LedText.TextAlignment = TextAlignment.Center;
+                    break;
+            }
+        }
+    }
+
     private static void IsTruePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
-        if (d is XamlLED c && e.NewValue is bool isTrue)
+        if (d is XamlLED led && e.NewValue is bool isTrue && led.LedOnColors.Count == led.LedOffColors.Count)
         {
-            if (c.LedOnColors.Count == 1 && c.LedOffColors.Count == 1)
+            led.ActiveLed = -1;
+            led.IsTrue = isTrue;
+            if (isTrue)
             {
-                c.IsTrue = isTrue;
-                if (isTrue)
-                {
-                    c.On();
-                }
-                else
-                {
-                    c.Off();
-                }
+                led.On();
+            }
+            else
+            {
+                led.Off();
             }
         }
     }
 
     private static void LedOnColorsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
-        if (d is XamlLED c && e.NewValue is List<Color> colors)
+        if (d is XamlLED led && e.NewValue is List<Color> colors)
         {
-            c.LedOnColors = colors;
-            c.LoadLeds(d, null!);
+            led.LedOnColors = colors;
+            led.LoadLeds(d, null!);
         }
     }
 
     private static void LedOffColorsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
-        if (d is XamlLED c && e.NewValue is List<Color> colors)
+        if (d is XamlLED led && e.NewValue is List<Color> colors)
         {
-            c.LedOffColors = colors;
-            c.LoadLeds(d, null!);
+            led.LedOffColors = colors;
+            led.LoadLeds(d, null!);
         }
     }
 
@@ -208,11 +401,11 @@ public class XamlLED : ContentControl
     /// <param name="e">The DependencyPropertyChangedEventArgs.</param>
     private static void ActiveLedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
-        if (d is XamlLED c && e.NewValue is int activeLed)
+        if (d is XamlLED led && e.NewValue is int activeLed)
         {
-            c.ActiveLed = activeLed;
-            c.Off();
-            c.On();
+            led.ActiveLed = activeLed;
+            led.Off();
+            led.On();
         }
     }
 
@@ -236,77 +429,64 @@ public class XamlLED : ContentControl
     /// <param name="e">Routed event atguments.</param>
     private void LoadLeds(object sender, RoutedEventArgs e)
     {
-        var panel = new StackPanel();
-        Content = panel;
-        panel.Orientation = LedOrientation;
-        panel.Children.Clear();
+        _LedText.Text = Text;
+        _LedText.Foreground = Foreground;
+        _LedText.FontWeight = FontWeight;
+        _LedText.FontSize = FontSize;
+
+        _ledStackPanel.Orientation = LedOrientation;
+        _ledStackPanel.Children.Clear();
         _leds.Clear();
-        double size;
-
-        if (LedOnColors.Count == 0)
-        {
-            LedOnColors.Add(Colors.Green);
-            LedOffColors.Add(Colors.Red);
-        }
-
-        if (LedOrientation == Orientation.Horizontal)
-        {
-            size = Height;
-        }
-        else
-        {
-            size = Width;
-        }
-
-        // Give it some size if forgotten to define width or height in combination with orientation
-        if (size.Equals(double.NaN) && (Parent is FrameworkElement parent) && (LedOnColors.Count != 0))
-        {
-            if (!double.IsNaN(parent.ActualWidth))
-            {
-                size = parent.ActualWidth / LedOnColors.Count;
-            }
-            else if (!double.IsNaN(parent.ActualHeight))
-            {
-                size = parent.ActualHeight / LedOnColors.Count;
-            }
-        }
 
         foreach (var color in LedOnColors)
         {
-            var ellipse = new Ellipse
-            {
-                Name = $"ellipses{_leds.Count}",
-                Height = size > 4 ? size - 4 : size,
-                Width = size > 4 ? size - 4 : size,
-                Margin = new Thickness(2),
-                Style = null
-            };
-
             // Border for led
             var srgb = new RadialGradientBrush(new GradientStopCollection
             {
                 new GradientStop(Color.FromArgb(255, 211, 211, 211), 0.8d),
                 new GradientStop(Color.FromArgb(255, 169, 169, 169), 0.9d),
                 new GradientStop(Color.FromArgb(255, 150, 150, 150), 0.95d),
-            });
+            })
+            {
+                GradientOrigin = new Point(0.5d, 0.5d),
+                Center = new Point(0.5d, 0.5d),
+                RadiusX = 0.5d,
+                RadiusY = 0.5d
+            };
 
-            ellipse.StrokeThickness = size / 20.0;
-
-            srgb.GradientOrigin = new Point(0.5d, 0.5d);
-            srgb.Center = new Point(0.5d, 0.5d);
-            srgb.RadiusX = 0.5d;
-            srgb.RadiusY = 0.5d;
-            ellipse.Stroke = srgb;
-
-            // Color of led
-            var rgb = GetLedColor(color);
-            ellipse.Fill = rgb;
+            var ellipse = new Ellipse
+            {
+                Name = $"ellipses{_leds.Count}",
+                Height = LEDSize > 4 ? LEDSize - 4 : LEDSize,
+                Width = LEDSize > 4 ? LEDSize - 4 : LEDSize,
+                Margin = new Thickness(2),
+                Style = null,
+                StrokeThickness = LEDSize / 20.0,
+                Fill = GetLedColor(color),
+                Stroke = srgb
+            };
             ellipse.Fill.Opacity = OffOpacity;
-            panel.Children.Add(ellipse);
+
+            _ledStackPanel.Children.Add(ellipse);
             _leds.Add(ellipse);
         }
 
-        Off();
+        if (ActiveLed == -1 && LedOnColors.Count == LedOffColors.Count)
+        {
+            if (IsTrue)
+            {
+                On();
+            }
+            else
+            {
+                Off();
+            }
+        }
+
+        if (ActiveLed > -1)
+        {
+            On();
+        }
     }
 
     /// <summary>
@@ -314,7 +494,7 @@ public class XamlLED : ContentControl
     /// </summary>
     private void On()
     {
-        if (LedOffColors.Count == LedOnColors.Count)
+        if (ActiveLed == -1 && LedOffColors.Count == LedOnColors.Count)
         {
             for (var i = 0; i < _leds.Count; i++)
             {
@@ -346,7 +526,7 @@ public class XamlLED : ContentControl
     /// </summary>
     private void Off()
     {
-        if (LedOffColors.Count == LedOnColors.Count)
+        if (ActiveLed == -1 && LedOffColors.Count == LedOnColors.Count)
         {
             for (var i = 0; i < _leds.Count; i++)
             {
